909e6c3930d438bab0992e55223d9f08c9ebed2e
stack <18821766778@163.com>
2021-05-26 23:01:38 +0800

    [add] add hello os

---
+++ hello_os/main.c
@@ -0,0 +1,7 @@
+// luyu @2021-05-26
+
+#include "vgastr.h"
+void main(){
+	printf("Hello OS!");
+	return;
+}
---
+++ hello_os/vgastr.h
@@ -0,0 +1,4 @@
+//luyu @2021-05-26
+
+void _strwrite(char* string);
+void printf(char* fmt, ...);
