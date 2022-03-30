79e8f52022d49082df72aa3d477c574319c3b3bb
fuscoyu <fuscoyu@gmail.com>
2022-03-30 10:09:04 +0800

    <feat>(gloabl): Updated some configurations and removed bad plugins

---
+++ wsl2/env.sh
@@ -0,0 +1,15 @@
+alias vi=nvim
+alias vim=nvim
+alias cddoc='cd /mnt/c/Users/leifeng/iCloudDrive/Documents/ICS'
+alias cdgo='cd ~/github/self/learning_go'
+
+alias cdk='cd ~/work/kolla/kolla-ansible'
+alias cdi='cd ~/work/kolla/installer'
+alias cdn='cd ~/work/openstack/nova'
+alias cdc='cd ~/work/openstack/cyborg'
+alias cdb='cd ~/work/build/'
+
+export http_proxy=http://192.168.1.197:10809
+export https_proxy=http://192.168.1.197:10809
+
+export PATH=$PATH:/usr/local/go/bin
